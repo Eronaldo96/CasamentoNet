64c91747fa98262ec07c9be3c57aa3af0094a11e
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2024-03-25 16:07:51 -0300

    subindo

--- src/app/pages/Homepage/Routes/index.js
+++ src/app/pages/Homepage/Routes/index.js
@@ -6,8 +6,12 @@ export default function HomePageRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
+      <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" />} />     
+      <Route path="/CasamentoNet/home" element={<Navigate replace to="/CasamentoNet/home/" />} />     
+      <Route path="/CasamentoNet/home/" element={<HomePage />} />
+      {/* <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
       <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" />} />      
-      <Route path="/CasamentoNet/home" element={<HomePage />} />
+      <Route path="/CasamentoNet/home" element={<HomePage />} /> */}
     </Routes>
   );
 };

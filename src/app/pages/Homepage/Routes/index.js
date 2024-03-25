4ee5c54c0de37959801fe526198f1445021d877e
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2024-03-25 16:11:29 -0300

    ajustes no vite

--- src/app/pages/Homepage/Routes/index.js
+++ src/app/pages/Homepage/Routes/index.js
@@ -5,13 +5,13 @@ import HomePage from '../index';
 export default function HomePageRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
+      {/* <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
       <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" />} />     
       <Route path="/CasamentoNet/home" element={<Navigate replace to="/CasamentoNet/home/" />} />     
-      <Route path="/CasamentoNet/home/" element={<HomePage />} />
-      {/* <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
+      <Route path="/CasamentoNet/home/" element={<HomePage />} /> */}
+      <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
       <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" />} />      
-      <Route path="/CasamentoNet/home" element={<HomePage />} /> */}
+      <Route path="/CasamentoNet/home" element={<HomePage />} />
     </Routes>
   );
 };

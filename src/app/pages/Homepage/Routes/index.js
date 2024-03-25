f7c42f638fcf0da1f85a2910016009189336490a
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2024-03-25 15:48:44 -0300

    ajustes das rotas

--- src/app/pages/Homepage/Routes/index.js
+++ src/app/pages/Homepage/Routes/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../index';
 
-export default function HomePageRoutes () {
+export default function HomePageRoutes() {
   return (
-    <Routes>      
-      <Route path="/" element={<Navigate replace to="/CasamentoNet/home" /> } />  
-      <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" /> } />     
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/CasamentoNet/home" />} />
+      <Route path="/CasamentoNet" element={<Navigate replace to="/CasamentoNet/home" />} />      
       <Route path="/CasamentoNet/home" element={<HomePage />} />
     </Routes>
   );
 };
-

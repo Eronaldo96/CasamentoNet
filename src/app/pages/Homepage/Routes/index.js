401e5fd307a7c6a6581ab7aa980f822e3f5cd56d
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2024-04-16 23:45:51 -0300

    adicionando componente grid

--- src/app/pages/Homepage/Routes/index.js
+++ src/app/pages/Homepage/Routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../index';
-import ListaPresentesRoutes from '../../ListaPresentes';
 
 export default function HomePageRoutes() {
   return (
--- src/app/pages/ListaPresentes/index.js
+++ src/app/pages/ListaPresentes/index.js
@@ -1,12 +1,35 @@
-// import { Grid, Card, CardMedia, Typography } from "@mui/material";
 import React, { useState } from "react";
+import { Input } from "@mui/material"; // Adicione esta linha
+import AirFry from "../../assets/images/AirFry.jpg";
 import "./styles.scss";
+import ResponseGrid from "../../components/ResponsiveGrid/index";
 
 export default function ListaPresentes() {
+  const [presentes, setPresentes] = useState([
+    { descricao: "Presente 1", imagem: AirFry },
+    { descricao: "Presente 2", imagem: AirFry },
+    // Adicione quantos presentes iniciais desejar
+  ]);
 
+
+  // const handleDescricaoChange = () => {
+  //   // Adiciona um novo presente ao array de presentes
+  //   setPresentes([{ descricao: "Teste", imagem: AirFry }, ...presentes]);
+  // };
+  
   return (
-    <div className="">
-        <h1>TESTE</h1>
+    <div className="container">
+      <div className="col-sm-12">
+        {/* Adiciona o componente Input e manipulador de eventos para alterar o estado */}
+        {/* <Input
+          value={presentes.descricao}
+          onChange={handleDescricaoChange}
+          placeholder="Descrição do Presente"
+        /> */}
+
+        {/* Exibe o componente ResponseGrid passando a lista de presentes como dados */}
+        <ResponseGrid data={presentes} />
+      </div>
     </div>
   );
 }

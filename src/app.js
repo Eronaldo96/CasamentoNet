da7d6e58341067e468a0dc42f8d505ac7e7d067d
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2024-04-17 15:08:15 -0300

    ajustes na tela

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import HomePageRoutes from './app/pages/Homepage/Routes/index';
-import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from './app/components/NavBar';
-import ListaPresentesRoutes from './app/pages/ListaPresentes/Routes';
+import React from "react";
+import HomePageRoutes from "./app/pages/Homepage/Routes/index";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./app/components/NavBar";
+import ListaPresentesRoutes from "./app/pages/ListaPresentes/Routes";
 
 export default function App() {
   return (
     <Router>
-      <NavBar />
-      <HomePageRoutes />
-      <ListaPresentesRoutes/>
+      <div style={{display:"flex", justifyContent: "center"}}>
+        <NavBar />
+        <div>
+          <HomePageRoutes />
+          <ListaPresentesRoutes />
+        </div>
+        <footer class="footerStyle">
+          <p>© 2024 Ero Company, Inc.</p>
+          <p class="float-right">
+            <a href="/">Back to top</a>
+          </p>
+        </footer>
+      </div>
     </Router>
   );
 }

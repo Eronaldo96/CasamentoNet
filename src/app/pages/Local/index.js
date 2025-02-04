45eef3b2148f9db70d03ede115b9dca1ac948168
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2025-02-03 23:10:05 -0300

    ajustando imagens

--- src/app/pages/Local/index.js
+++ src/app/pages/Local/index.js
@@ -2,6 +2,8 @@ import React, { useState, useRef } from "react";
 import CapelaSaoPedro from "../../assets/images/Capela de São Pedro.jpg";
 import CapelaSaoPedroInterno from "../../assets/images/Capela de São Pedro Interno.jpg";
 import CapelaSaoPedroExterno from "../../assets/images/Capela de São Pedro Externo.jpg";
+import YuccaEntrada from "../../assets/images/yucca entrada.jpg";
+import YuccaLogo from "../../assets/images/yucca entrada 2.jpg";
 
 import "./styles.scss";
 import StandardImageList from "../../components/SatndardImageList/index";
@@ -19,6 +21,17 @@ export default function Local() {
     {
       descricao: "Área Externa",
       imagem: CapelaSaoPedroExterno,
+    },
+  ]);
+
+  const [localBrunch] = useState([
+    {
+      descricao: "Yucca",
+      imagem: YuccaLogo,
+    },
+    {
+      descricao: "Yucca Entrada",
+      imagem: YuccaEntrada,
     },
   ]);
   const localSection = useRef(null);
@@ -41,19 +54,22 @@ export default function Local() {
         </div>
 
         <hr className="decorative-line" />
-        
+
         <div className="TituloPagina">
-          <h1>Local do Brunch</h1>
-          <h5 className="LocalInfoPagina">
-            Casa Portuguesa
-          </h5>
+          <h1>Local da Recepção</h1>
+          <h7>
+            Após a cerimônia haverá um brunch para celebrarmos o grande dia.
+          </h7>
+
+          <h5 style={{marginTop:"2%"}} className="LocalInfoPagina">Yucca gastronomia</h5>
         </div>
         <div>
-          <StandardImageList data={local} />
+          <StandardImageList data={localBrunch} />
         </div>
         <div className="LocalInfoPagina">
           <h6>
-            Endereço: Av. Beira Mar, 4600 - Mucuripe, Fortaleza - CE, 60165-121
+            Endereço: Rua Frei Mansueto, 160 - Meireles, Fortaleza - CE,
+            60175-070
           </h6>
         </div>
       </div>

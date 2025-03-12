32711c68458ddb191237e36f896a19fe25664ca7
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2025-03-11 23:56:10 -0300

    melhoria no footer

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import "./styles.scss";
 
 export default function Footer() {
   return (
     <div className="footerStyle">
-      <p>© 2024 Ero Company, Inc.</p>
-      <p className="float-right">
-        <Button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} variant="outlined">Voltar pro topo</Button>
-      </p>
+      <div className="marca">
+        <p>© 2024 Ero Company, Inc.</p>
+      </div>
+      <div>
+        <p className="float-right">
+          <Button
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            variant="outlined"
+          >
+            Voltar pro topo
+          </Button>
+        </p>
+      </div>
     </div>
   );
 }
